0acee05deb82b3cfb4e2d1670483fe225232cf21
tomoya0417 <batayan0281@gmail.com>
2021-07-30 23:01:35 +0900

    localhost

--- src/Component/ComponentA.js
+++ src/Component/ComponentA.js
@@ -7,12 +7,21 @@ const ComponentA = () =>{
         console.log('Hello world');
       }
 
+      const clickMe = () => {
+        window.alert('Hello world')
+      }
+
 return(
 
     <div>
         <div>
         <h1>level 1</h1>
         <button onClick={Hello}>Hello World</button></div>
+
+        <div>
+        <h1>level 2</h1>
+        <button onClick={clickMe}>Hello World2</button></div>
+
     </div>
 
 )

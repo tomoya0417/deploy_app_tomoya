3960c7099e41e36b3b1effa1cb501024640199d9
tomoya0417 <batayan0281@gmail.com>
2021-07-31 16:00:38 +0900

    count

--- src/Component/ComponentA.js
+++ src/Component/ComponentA.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 
 const ComponentA = () =>{
+    const [count,setCount] = useState(0)
 
     const Hello = () => {
         console.log('Hello world');
       }
 
-      const clickMe = () => {
+    const clickMe = () => {
         window.alert('Hello world')
+      }
+    
+    const increment = () => {
+        setCount(count + 1)
+      }
+
+    const decrement = () => {
+        setCount(count - 1)
+      }
+    　
+    const reset = () => {
+        setCount(0)
       }
 
 return(
@@ -21,6 +34,14 @@ return(
         <div>
         <h1>level 2</h1>
         <button onClick={clickMe}>Hello World2</button></div>
+
+        <div>
+        <h1>level 3</h1>
+        <button onClick={increment}>いいね!</button>
+        <button onClick={decrement}>良くないね!</button>
+        <button onClick={reset}>reset</button>
+        {count}
+        </div>
 
     </div>
 

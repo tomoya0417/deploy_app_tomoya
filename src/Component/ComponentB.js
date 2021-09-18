aff1af0cebeaea94d1e5a4238cdc9399428dca8d
tomoya0417 <batayan0281@gmail.com>
2021-09-18 14:52:45 +0900

    page

--- src/Component/ComponentB.js
+++ src/Component/ComponentB.js
@@ -1,12 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import { Link } from "react-router-dom";
 
 const ComponentB = () =>{
 
+
 return(
+<div>  
+ <Link to="ComponentA">ComponentA</Link> 
     <div>
        <h1>level 8</h1>
     </div>
+</div>
 )
 }
 export default ComponentB

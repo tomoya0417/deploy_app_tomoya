10311214b560dccf4466b8bc82ecab7a8fe28ed0
Hattori-Takuma <hattori.ta@icloud.com>
2021-10-02 16:07:11 +0900

    params

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import ComponentA from './Component/ComponentA'
 import ComponentB from './Component/ComponentB'
-import PageA from './PAGE/PageA';
-import PageB from './PAGE/PageB';
+import PageA from './PAGE/pageA';
+import PageB from './PAGE/pageB';
 
 import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  const histry =useHistory();
-  return (  
-    <Router histry ={ histry }>
-    <Switch>
-      <Route exact path="/" >
-        <ComponentA />
-      </Route>
-      <Route exact path="/ComponentB" >
-        <ComponentB />
-      </Route>
-      <Route exact path="/PageA" >
-        <PageA />
-      </Route>
-      <Route exact path="/PageB" >
-        <PageB />
-      </Route>
-    </Switch>
-  </Router>
+  const histry = useHistory();
+  return (
+    <Router histry={histry}>
+      <Switch>
+        <Route exact path="/" >
+          <ComponentA />
+        </Route>
+        <Route exact path="/ComponentB" >
+          <ComponentB />
+        </Route>
+        <Route exact path="/PageA/" >
+          <PageA />
+        </Route>
+        <Route exact path="/pageB" >
+          <PageB />
+        </Route>
+
+        <Route exact path="/PageB/:params" >
+          <PageB />
+        </Route>
+
+      </Switch>
+    </Router>
   )
 }
 
--- src/PAGE/pageB.js
+++ src/PAGE/pageB.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import { useParams } from　'react-router-dom'
+import { useParams } from 'react-router-dom'
 
-const PageB = () =>{
+const PageB = () => {
 
-return(
+  const params = useParams();
+
+  return (
     <div>
       <Link to="/">ComponentA</Link>
       <Link to="ComponentB">ComponentB</Link>
-      <Link to="PageA">PageA</Link> 
+      <Link to="/PageA">PageA</Link>
 
       <h1>level11</h1>
+      <h2>{params.params}</h2>
     </div>
-)
+  )
 }
 
 export default PageB
